53ace4fe33e5daf6c706655a24839bd801b0358e
koolbear-oss <mattias.colla@gmail.com>
2025-08-24 17:03:54 +0200

    payment webhook errors

--- .netlify/functions/payment-webhook.js
+++ .netlify/functions/payment-webhook.js
@@ -11,23 +11,32 @@ exports.handler = async (event, context) => {
 
     const supabaseUrl = process.env.SUPABASE_URL;
     const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-    // Supabase client met service role key, zodat we data kunnen updaten
     const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
-    
-    try {
-        const { id: paymentId } = JSON.parse(event.body);
 
-        // Initialiseer Mollie client
+    try {
+        let paymentId;
+        const contentType = event.headers['content-type'];
+
+        if (contentType && contentType.includes('application/x-www-form-urlencoded')) {
+            const params = new URLSearchParams(event.body);
+            paymentId = params.get('id');
+        } else {
+            // Valideer voor Mollie webhooks die mogelijk JSON-data sturen
+            const body = JSON.parse(event.body);
+            paymentId = body.id;
+        }
+
+        if (!paymentId) {
+            return { statusCode: 400, body: 'Missing payment ID.' };
+        }
+
         const mollie = mollieClient({ apiKey: process.env.MOLLIE_API_KEY });
         const molliePayment = await mollie.payments.get(paymentId);
         
-        // Controleer of de betaling betaald is
         if (molliePayment.isPaid()) {
             const { giftId, payerEmail } = molliePayment.metadata;
             const paidAmount = parseFloat(molliePayment.amount.value);
             
-            // Haal de huidige gift op om het bedrag te bepalen
             const { data: gift, error: fetchError } = await supabase
                 .from('gifts')
                 .select('current_amount')
@@ -39,10 +48,8 @@ exports.handler = async (event, context) => {
                 return { statusCode: 404, body: 'Cadeau niet gevonden.' };
             }
             
-            // Bereken het nieuwe totale bedrag
             const newAmount = gift.current_amount + paidAmount;
             
-            // Update de 'current_amount' in de 'gifts' tabel
             const { error: updateError } = await supabase
                 .from('gifts')
                 .update({ current_amount: newAmount })
@@ -53,7 +60,6 @@ exports.handler = async (event, context) => {
                 return { statusCode: 500, body: 'Database update mislukt.' };
             }
 
-            // Voeg de betaling toe aan de 'payments' tabel
             await supabase
                 .from('payments')
                 .insert([{
